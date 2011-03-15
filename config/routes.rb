33bfc5f91c8558d68bbb7159dd062bc41e857ab7
Dearest <dearest@dearest-laptop.(none)>
2011-03-15 13:37:31 -0600

    WAYYY long past overdue commit. Haven't been using this enough. Project well more advanced at this point

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/contact"
+	resources :users
+	
+  #get "users/new"
   
-  get "pages/about"
+  match '/signup', :to => 'users#new'
 
+	match '/contact', :to => 'pages#contact'
+	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
+	
+	root :to => 'pages#home'
+	
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
