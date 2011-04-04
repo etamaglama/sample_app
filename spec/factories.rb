c5e9eed0d8f17d974cdde8ca8ad7cc16f16da41b
Dearest <dearest@dearest-laptop.(none)>
2011-04-03 21:24:21 -0600

    Added user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,3 +10,14 @@ Factory.sequence :email do |n|
 	"factoried.user-#{n}@testing.com"
 end
 
+Factory.define :micropost do |micropost|
+	micropost.content "Foo bar"
+	micropost.association :user
+end
+
+
+
+=begin
+
+=end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,6 +102,7 @@ describe User do
 			end
 		end
 		
+		
 		describe "authenticate method" do
 			it "should return nil on email/password mismatch" do
 				wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
@@ -120,6 +121,7 @@ describe User do
 		end
 	end
 	
+	
 	describe "admin attribute" do
 		before(:each) do
 			@user = User.create!(@attr)
@@ -136,6 +138,47 @@ describe User do
 		it "should be convertible to an admin" do
 			@user.toggle!(:admin)
 			@user.should be_admin
+		end
+	end
+	
+	
+	describe "micropost associations" do
+		before(:each) do
+			@user = User.create!(@attr)
+			@mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+			@mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+		end
+		
+		it "should have a microposts attribute" do
+			@user.should respond_to(:microposts)
+		end
+		
+		it "should have the microposts in the right order" do
+			@user.microposts.should == [@mp2, @mp1]
+		end
+		
+		it "should destroy associated microposts" do
+			@user.destroy
+			[@mp1, @mp2].each do |micropost|
+				Micropost.find_by_id(micropost.id).should be_nil
+			end
+		end
+		
+		describe "status feed" do
+			it "should have a feed" do
+				@user.should respond_to(:feed)
+			end
+			
+			it "should include the user's microposts" do
+				@user.feed.include?(@mp1).should be_true
+				@user.feed.include?(@mp2).should be_true
+			end
+			
+			it "should not include different users' microposts" do
+				mp3 = Factory(:micropost,
+											:user => Factory(:user, :email => Factory.next(:email)))
+				@user.feed.include?(mp3).should be_false
+			end
 		end
 	end
 end
